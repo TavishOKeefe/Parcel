17898746f3a763efcfa413f2f46bae0eaa774ad0
Alex Garcia and Tavish Okeefe <tavish@>
2018-11-27 16:38:50 -0800

    add files for tests.

--- Parcels/Controllers/FormController.cs
+++ Parcels/Controllers/FormController.cs
@@ -11,5 +11,16 @@ namespace Parcels.Controllers
     {
       return View();
     }
+    [HttpPost("/items")]
+    public ActionResult Form(string height, string length, string width, string weight)
+    {
+      Parcel yourParcel = new Parcel(height, length, width, weight);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
